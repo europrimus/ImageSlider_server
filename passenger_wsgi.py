1603f61839c161641396cb805d8a110a0f0be330
Didier Courty (Europrimus) <dev@europrimus.ninja>
2021-12-20 13:31:46 +0100

    return json

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,13 +1,15 @@
 import os
 import sys
-
+import json
 
 sys.path.insert(0, os.path.dirname(__file__))
 
 
 def application(environ, start_response):
-    start_response('200 OK', [('Content-Type', 'text/plain')])
-    message = 'It works!\n'
-    version = 'Python %s\n' % sys.version.split()[0]
-    response = '\n'.join([message, version])
-    return [response.encode()]
+    logging.debug("environ : {}".format(environ))
+    jsonData={}
+    jsonData['scriptFile'] = os.path.basename(__file__)
+    jsonData['requestPath'] = environ['REQUEST_URI']
+    jsonData['pythonVersion'] = sys.version.split()[0]
+    start_response('200 OK', [('Content-Type', 'application/json')])
+    return [json.dumps(jsonData).encode()]
