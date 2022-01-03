07a18f4f080b2e1ffc01763b289a34113bde2332
Didier Courty (Europrimus) <dev@europrimus.ninja>
2022-01-03 22:31:11 +0100

    Add HTTPS support

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -10,7 +10,15 @@ def application(environ, start_response):
     logger = Logger(__name__)
     logger.info("Start application")
     listImg = ListImg(environ['REQUEST_URI'])
-    jsonData = listImg.list(environ['SCRIPT_URI'])
+    if 'SCRIPT_URI' in environ :
+        baseUrl = environ['SCRIPT_URI']
+    else :
+        baseUrl = "{}://{}{}".format(
+            "https" if environ['SERVER_PORT'] == '443' else "http",
+            environ['SERVER_NAME'],
+            environ['REQUEST_URI']
+            )
+    jsonData = listImg.list(baseUrl)
     del logger
     start_response('200 OK', [('Content-Type', 'application/json')])
     return [json.dumps(jsonData).encode()]
